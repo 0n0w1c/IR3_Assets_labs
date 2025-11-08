edfb78da2daa584385eb996f5a0e72fc1306a9ab
0n0w1c <47831850+0n0w1c@users.noreply.github.com>
2025-11-08 15:17:42 -0600

    Version 0.0.8

--- data-updates.lua
+++ data-updates.lua
@@ -16,13 +16,37 @@ if mods["space-age"] and settings.startup["IR3-reskin-biolab"].value == true the
     require("prototypes/technology/biolab")
 else
     if settings.startup["IR3-lab-style"].value == "quantum lab" then
-        require("prototypes/explosion/lab")
+        require("prototypes/explosion/quantum-lab")
         require("prototypes/entity/quantum-lab")
         require("prototypes/item/quantum-lab")
     else
         require("prototypes/explosion/lab")
         require("prototypes/entity/lab")
         require("prototypes/item/lab")
+    end
+end
+
+if mods["quality"] then
+    local recycling = require("__quality__/prototypes/recycling")
+    local recipe
+
+    recipe = data.raw["recipe"]["lab"]
+    if recipe then recycling.generate_recycling_recipe(recipe) end
+
+    recipe = data.raw["recipe"]["biolab"]
+    if settings.startup["IR3-reskin-biolab"].value == true then
+        if recipe then
+            recipe.icon = nil
+            recipe.icon_size = nil
+            recipe.icons = {
+                {
+                    icon = "__IndustrialRevolution3Assets1__/graphics/icons/64/quantum-lab.png",
+                    icon_size = 64,
+                }
+            }
+
+            recycling.generate_self_recycling_recipe(recipe)
+        end
     end
 end
 
